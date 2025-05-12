c97298c4997a37922abccbcde0af95710054f9b0
Kevin Penner <crazyapple48@gmail.com>
2025-05-12 09:22:15 -0700

    Style ComboBox and add to New Recipe page

--- Pennerooney_Cookbook/ViewModels/NewRecipePageViewModel.cs
+++ Pennerooney_Cookbook/ViewModels/NewRecipePageViewModel.cs
@@ -1,8 +1,54 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Pennerooney_Cookbook.Data;
+using Pennerooney_Cookbook.Models;
 
 namespace Pennerooney_Cookbook.ViewModels;
 
-public class NewRecipePageViewModel() : PageViewModel(PageNames.NewRecipe)
+public partial class NewRecipePageViewModel : PageViewModel
 {
+    [ObservableProperty] private ObservableCollection<Genre> _genres;
+    [ObservableProperty] private ObservableCollection<Method> _methods;
+    [ObservableProperty] private ObservableCollection<Purpose> _purposes;
+
+    public NewRecipePageViewModel() : base(PageNames.NewRecipe)
+    {
+        GetMethods();
+        GetGenres();
+        GetPurposes();
+    }
     
+    [RelayCommand]
+    private void GetMethods()
+    {
+        // TODO: Send this logic to a service to pull methods from db
+        Methods =
+        [
+            new Method(0, "InstaPot"),
+            new Method(1, "Stove")
+        ];
+    }    
+    
+    [RelayCommand]
+    private void GetGenres()
+    {
+        // TODO: Send this logic to a service to pull genres from db
+        Genres =
+        [
+            new Genre(0, "Mexican"),
+            new Genre(1, "American")
+        ];
+    }    
+    
+    [RelayCommand]
+    private void GetPurposes()
+    {
+        // TODO: Send this logic to a service to pull purposes from db
+        Purposes =
+        [
+            new Purpose(0, "Entree"),
+            new Purpose(1, "Dessert")
+        ];
+    }
 }
