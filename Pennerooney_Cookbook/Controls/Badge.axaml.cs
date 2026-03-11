7a07429da2a7fdeaa295fc352f96f091abf3792f
Kevin Penner <111170452+crazyapple48@users.noreply.github.com>
2026-03-10 21:00:55 -0700

    New font working

---
+++ Pennerooney_Cookbook/Controls/Badge.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Pennerooney_Cookbook.Controls;
+
+public partial class Badge : UserControl
+{
+    public Badge()
+    {
+        InitializeComponent();
+    }
+}
